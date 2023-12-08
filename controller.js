f6fd2e93e066dfdd48290b207f83af826983119c
Bradley <104906904+bradley-03@users.noreply.github.com>
2023-12-08 17:31:09 +0000

    added rendering mode values

--- controller.js
+++ controller.js
@@ -7,6 +7,12 @@ const {dialog} = require('electron')
 
 let cfgFile = ""
 
+const renderAPIs = {
+    performance: {PreferredRHI: "dx11", PreferredFeatureLevel: "es31"},
+    dx11: {PreferredRHI: "dx11", PreferredFeatureLevel: "sm5"},
+    dx12: {PreferredRHI: "dx12", PreferredFeatureLevel: "sm6"},
+}
+
 module.exports.openCfgFile = async function () {
     const configSettings = await config.getConfig()
     if (fs.existsSync(path.join(configSettings.filePath, "GameUserSettings.ini"))) {
@@ -18,10 +24,23 @@ module.exports.openCfgFile = async function () {
 }
 module.exports.setValues = async function () {
     const configSettings = await config.getConfig()
+
+    let renderApi = "dx12"
+    if (cfgFile['D3DRHIPreference'].PreferredRHI == "dx11") {
+        if (cfgFile['D3DRHIPreference'].PreferredFeatureLevel == "es31") {
+            renderApi = "performance"
+        } else {
+            renderApi = "dx11"
+        }
+    } else {
+        renderApi = "dx12"
+    }
+
     const values = {
         ...configSettings,
         resolutionX: cfgFile['/Script/FortniteGame']['FortGameUserSettings'].ResolutionSizeX || 1920,
         resolutionY: cfgFile['/Script/FortniteGame']['FortGameUserSettings'].ResolutionSizeY || 1080,
+        renderApi,
     }
 
     return values
--- renderer.js
+++ renderer.js
@@ -2,12 +2,14 @@ const filepathBtn = document.getElementById('filepath-btn')
 const filepathBox = document.getElementById('filepath-box')
 const resXBox = document.getElementById('resXBox')
 const resYBox = document.getElementById('resYBox')
+const renderSelect = document.getElementById('render-select')
 
 async function setValues () {
     const values = await window.electronAPI.setValues()
     filepathBox.value = values.filePath
     resXBox.value = values.resolutionX
     resYBox.value = values.resolutionY
+    renderSelect.value = values.renderApi
 }
 setValues()
 

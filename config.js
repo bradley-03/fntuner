3d4029c55518fddef1dce865951b2307f3106b13
Bradley <104906904+bradley-03@users.noreply.github.com>
2023-12-08 16:33:54 +0000

    filepath error handling

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const {dialog} = require('electron')
 const config = require('electron-settings')
 const os = require('os')
 const path = require('path')
+const fs = require('fs')
 
 module.exports.createConfig = function () {
     if (!config.hasSync("settings")) {
@@ -19,10 +20,18 @@ module.exports.initConfig = function () {
 module.exports.setFilePath = async function () {
     const {canceled, filePaths} = await dialog.showOpenDialog({properties: ['openDirectory']})
     if (!canceled) {
-        await config.set('settings', {filePath: filePaths[0]})
-        return filePaths
+        if (fs.existsSync(path.join(filePaths[0], 'GameUserSettings.ini'))) {
+            await config.set('settings', {filePath: filePaths[0]})
+            return filePaths
+        }
+        await dialog.showErrorBox("Invalid Directory!", "The directory you selected doesn't include a 'GameUserSettings.ini' config file.")
+        return config.getSync('settings.filePath')
     }
     return config.getSync('settings.filePath')
 }
 
+module.exports.getConfig = async function () {
+    const output = await config.get("settings")
+    return output
+}
 

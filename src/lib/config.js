0c05a7042a7cbef2123432f7684739e36bb1cf71
Bradley <104906904+bradley-03@users.noreply.github.com>
2023-12-26 19:46:52 +0000

    improved current config info

--- src/lib/config.js
+++ src/lib/config.js
@@ -11,8 +11,33 @@ module.exports.getCurrentCfg = async function () {
     try {
         const originalData = await fs.readFile(cfgFilePath, 'utf-8')
         const iniData = ini.parse(originalData) // parse to object
+    
+        // parse render api
+        let renderApi
+        if (iniData['D3DRHIPreference'].PreferredRHI == "dx11") {
+            if (iniData['D3DRHIPreference'].PreferredFeatureLevel == "es31") {
+                renderApi = "performance"
+            } else {
+                renderApi = "dx11"
+            }
+        } else {
+            renderApi = "dx12"
+        }
 
-        return iniData
+        // get resolution
+        const resolutions = {
+            X: iniData['/Script/FortniteGame']['FortGameUserSettings'].ResolutionSizeX,
+            Y: iniData['/Script/FortniteGame']['FortGameUserSettings'].ResolutionSizeY,
+        }
+
+        // get file attributes (for read only)
+        const fileAttrs = await winattr.getSync(cfgFilePath)
+
+        return {
+            renderApi,
+            resolutions,
+            readOnly: fileAttrs.readonly
+        }
     } catch (e) {
         console.error("Config file not found.", e)
         return null

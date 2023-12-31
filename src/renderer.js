534824c672757893e8fd942c317fa4e1686c85a7
Bradley <104906904+bradley-03@users.noreply.github.com>
2023-12-31 01:30:24 +0000

    added tooltips

--- src/renderer.js
+++ src/renderer.js
@@ -4,18 +4,20 @@ const resXbox = document.getElementById('resXBox')
 const resYbox = document.getElementById('resYBox')
 const renderApiSelect = document.getElementById('render-select')
 const readonlyCheck = document.getElementById('readonly-check')
-const submitBtn = document.getElementById('confirmbtn')
+const confirmBtn = document.getElementById('confirmbtn')
 const undoBtn = document.getElementById('undobtn')
 const closeBtn = document.getElementById('closebtn')
 const minimizeBtn = document.getElementById('minimizebtn')
+const tooltip = document.getElementById('tooltip')
+const readonlyContainer = document.getElementById('readonly-container')
 
-async function updateValues () {
+async function updateValues() {
     try {
         const data = await ipcRenderer.invoke('get-config')
 
         // update values with new data
         resXbox.value = data.resolutions.X
-        resYbox.value = data.resolutions.Y 
+        resYbox.value = data.resolutions.Y
         renderApiSelect.value = data.renderApi
         readonlyCheck.checked = data.readOnly
     } catch (e) {
@@ -24,7 +26,7 @@ async function updateValues () {
     }
 }
 
-async function submitConfigChanges () {
+async function submitConfigChanges() {
     const data = {
         configData: {
             '/Script/FortniteGame': {
@@ -37,9 +39,9 @@ async function submitConfigChanges () {
             }
         },
         renderApi: renderApiSelect.value,
-        readOnly: readonlyCheck.checked        
-    } 
-    
+        readOnly: readonlyCheck.checked
+    }
+
     try {
         const res = await ipcRenderer.invoke('write-config', data)
     } catch (e) {
@@ -47,7 +49,7 @@ async function submitConfigChanges () {
     }
 }
 
-submitBtn.addEventListener('click', async () => {
+confirmBtn.addEventListener('click', async () => {
     await submitConfigChanges()
 })
 
@@ -66,3 +68,28 @@ minimizeBtn.addEventListener('click', () => {
 document.addEventListener('DOMContentLoaded', () => {
     updateValues();
 });
+
+
+//tooltips
+confirmBtn.addEventListener('mouseenter', function () {
+    tooltip.innerHTML = "Confirm your changes"
+})
+
+confirmBtn.addEventListener('mouseleave', function () {
+    tooltip.innerHTML = ""
+})
+
+undoBtn.addEventListener('mouseenter', function () {
+    tooltip.innerHTML = "Reset values back to current config"
+})
+
+undoBtn.addEventListener('mouseleave', function () {
+    tooltip.innerHTML = ""
+})
+readonlyContainer.addEventListener('mouseenter', function () {
+    tooltip.innerHTML = "Set config file to read only (settings will revert when you restart game)"
+})
+
+readonlyContainer.addEventListener('mouseleave', function () {
+    tooltip.innerHTML = ""
+})
